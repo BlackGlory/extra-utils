405be6c7d7b7a5b4de1f91af448ebd33014018e2
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-11-21 17:05:32 +0800

    refactor: improve

--- src/node-js-stream.ts
+++ src/node-js-stream.ts
@@ -1,9 +1,16 @@
-import { readable, writable } from  'is-stream'
+// https://nodejs.org/docs/latest/api/stream.html#stream_api_for_stream_implementers
 
-export function isNodeJSWritableStream(val: unknown): val is NodeJS.WritableStream {
-  return writable(val)
+import { isObject } from './object'
+
+export function isNodeJSWritableStream(val: any): val is NodeJS.WritableStream {
+  return isObject(val)
+      && typeof val._write === 'function'
+      && typeof val._writev === 'function'
+      && typeof val._final === 'function'
 }
 
-export function isNodeJSReadableStream(val: unknown): val is NodeJS.ReadableStream {
-  return readable(val)
+export function isNodeJSReadableStream(val: any): val is NodeJS.ReadableStream {
+  return isObject(val)
+      && typeof val._read === 'function'
+      && typeof val.pipe === 'function'
 }
