909640269b88a82d43532d4150b3159261dd337b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-02-28 08:22:33 +0000

    feat: add higher-order types

--- src/interfaces/object.ts
+++ src/interfaces/object.ts
@@ -7,7 +7,18 @@ export type NonNullableKeys<T> = {
       : P
 }[keyof T]
 
+export type NullableKeys<T> = {
+  [P in keyof T]:
+    null extends T[P]
+    ? P
+    : undefined extends T[P]
+      ? P
+      : never
+}[keyof T]
+
 export type PickNonNullableProps<T> = Pick<T, NonNullableKeys<T>>
+
+export type PickNullableProps<T> = Pick<T, NullableKeys<T>>
 
 export type FunctionKeys<T> = {
   [P in keyof T]:
@@ -15,3 +26,16 @@ export type FunctionKeys<T> = {
     ? P
     : never
 }[keyof T]
+
+export type MapPropsType<T, OldType, NewType> = {
+  [P in keyof T]:
+    OldType extends T[P]
+    ? Exclude<T[P], OldType> | NewType
+    : T[P]
+}
+
+export type MapPropsNullToUndefined<T> = MapPropsType<T, null, undefined>
+
+export type NullablePropsToOptionalProps<T> =
+  PickNonNullableProps<T>
+& Partial<MapPropsNullToUndefined<PickNullableProps<T>>>
