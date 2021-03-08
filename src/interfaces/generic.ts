36ba9414996e85caa324cb8d8c3eb99dca01d5a8
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-03-08 12:10:03 +0000

    feat: add UnboxPromise, UnboxPromiseLike

--- src/interfaces/generic.ts
+++ src/interfaces/generic.ts
@@ -2,3 +2,6 @@ export type WithDefaultType<T, DefaultType> =
   T extends undefined | null
   ? NonNullable<T> | DefaultType
   : T
+
+export type UnboxPromise<T> = T extends Promise<infer U> ? U : never
+export type UnboxPromiseLike<T> = T extends PromiseLike<infer U> ? U : never
