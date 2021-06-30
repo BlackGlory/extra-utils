bff8c7c47da1564b983e7b21abc276314c395295
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-06-30 12:53:55 +0000

    fix: inEnum

--- src/enum.ts
+++ src/enum.ts
@@ -1,3 +1,6 @@
-export function inEnum<T extends object>(val: unknown, _enum: T): val is T {
+// 由于以下原因, 该函数不应该被改写为其他形式:
+// - enum在TypeScript里被作为单独的类型看待, 不满足extends object
+// - 作为泛型时, enum参数会被提取为typeof enum类型而不是enum
+export function inEnum<T>(val: unknown, _enum: object): val is T {
   return Object.values(_enum).includes(val)
 }
