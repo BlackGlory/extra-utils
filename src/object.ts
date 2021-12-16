6043e3dd565130e3a0086204f61e2e985bc3332c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-16 17:09:40 +0000

    feat: improve type assertions

--- src/object.ts
+++ src/object.ts
@@ -1,21 +1,21 @@
 import isPlainObjectByLodash from 'lodash.isplainobject'
 
-export function isObject(val: unknown): val is Record<string | symbol | number, unknown> {
+export function isObject(val: unknown): val is object & Record<string | symbol | number, unknown> {
   return val !== null
       && typeof val === 'object'
 }
 
-export function isntObject<T>(val: T): val is Exclude<T, Record<string | symbol | number, unknown>> {
+export function isntObject<T>(val: T): val is Exclude<T, object & Record<string | symbol | number, unknown>> {
   return !isObject(val)
 }
 
-export function isPlainObject(val: unknown): val is Record<string | symbol | number, unknown> {
+export function isPlainObject(val: unknown): val is object & Record<string | symbol | number, unknown> {
   // 仅通过`Object.getPrototypeOf(val) === Object.prototype`无法判断Node.js的一些纯对象,
   // 因此在此引入lodash的方法.
   return isPlainObjectByLodash(val)
 }
 
-export function isntPlainObject<T>(val: T): val is Exclude<T, Record<string | symbol | number, unknown>> {
+export function isntPlainObject<T>(val: T): val is Exclude<T, object & Record<string | symbol | number, unknown>> {
   return !isPlainObject(val)
 }
 
